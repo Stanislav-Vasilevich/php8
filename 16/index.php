6d319495be17b0264e71ed74031af2fa6c720efd
StanislaVailevich <stanislavasilevich@yandex.ru>
2025-03-11 17:21:16 +0300

    completed 17 homeworks

--- 16/index.php
+++ 16/index.php
@@ -3,59 +3,59 @@
 <!doctype html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Задача по циклу While</title>
-    <style>
-        td {
-            background: antiquewhite;
-        }
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Задача по циклу While</title>
+  <style>
+    td {
+      background: antiquewhite;
+    }
 
-        tr {
-            background: darkgray;
-        }
-    </style>
+    tr {
+      background: darkgray;
+    }
+  </style>
 </head>
 <body>
 <?php
-$year = 1990;
-
-echo "<select>";
-
-while($year <= 2024) {
-	echo "<option>" . $year;
-
-	$year++;
-
+  $year = 1990;
+  
+  echo "<select>";
+  
+  while ($year <= 2025) {
+    echo "<option>" . $year;
+    
+    $year++;
+    
     echo "</option>";
-}
-
-echo "</select>";
+  }
+  
+  echo "</select>";
 ?>
 
 <?php
-$tr = 1;
-
-echo "<table>";
-
-while($tr <= 10) {
-	$td = 1;
-
+  $tr = 1;
+  
+  echo "<table>";
+  
+  while ($tr <= 10) {
+    $td = 1;
+    
     echo "<tr>";
-
-    while($td <= 10) {
-        echo "<td>" . $tr * $td . "</td>";
-        $td++;
+    
+    while ($td <= 10) {
+      echo "<td>" . $tr * $td . "</td>";
+      $td++;
     }
-
+    
     echo "</tr>";
-
-	$tr++;
-}
-
-echo "</table>";
+    
+    $tr++;
+  }
+  
+  echo "</table>";
 ?>
 </body>
 </html>
