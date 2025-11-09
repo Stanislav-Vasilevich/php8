ba129d4f54a19d8e783135c988ea74efcceb2508
Stanislav <stanislavasilevich@gmail.com>
2025-11-09 16:46:10 +0300

    прошел урок по арифметическим операторам

--- 08/index.php
+++ 08/index.php
@@ -3,36 +3,38 @@
 <!doctype html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Урок 8</title>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Урок 8</title>
 </head>
 <body>
-	<h1>Арифметические операторы.</h1>
+<h1>Арифметические операторы.</h1>
 
 <?php
 $num = -32;
 
-echo -$num;
+echo '-$num: ', -$num;
 echo '<br>';
-var_dump('10'); // '10'
+var_dump('10'); // string(2) "10"
 echo '<br>';
-var_dump(+'10'); // 10
+var_dump(+'-10'); // int(-10)
 echo '<br>';
-var_dump(-'10'); // -10
+var_dump(-'-10'); // int(10)
 echo '<br>';
-var_dump(1 + '2t'); // warning и вычисление php >= 8 версии
+var_dump(-'10'); // int(-10)
+echo '<br>';
+var_dump(1 + '2t'); // Warning и вычисление php >= 8.0 версии и Notice php >= 7.0 и php < 8.0 версии будет int(3)
 // var_dump(1 + 't2'); // fatal error
 echo '<br>';
-var_dump(20 % 7); // 6
+var_dump(20 % 7); // int(6)
 echo '<br>';
-var_dump(-20 % 7); // -6
+var_dump(-20 % 7); // int(-6)
 echo '<br>';
-var_dump(20 % -7); // 6, если второе число отрицательное, все равно получаем положительное
+var_dump(20 % -7); // int(6), если второе число отрицательное, все равно получаем положительное
 echo '<br>';
-var_dump(2 ** 3); // 8
+var_dump(2 ** 3); // int(8)
 ?>
 </body>
 </html>
